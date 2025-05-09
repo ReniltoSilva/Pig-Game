a14d2ba9975b7047d63ffd547d01ee4568939dad
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-05-09 11:12:16 -0500

    Added roll dice, save current score, reset current and total score when press hold btn or when dice is egual 1, change players turn

--- starter/script.js
+++ starter/script.js
@@ -5,10 +5,11 @@
 ////If number between 2 and 6
 ////1.1 - Add number to the current field DONE
 ////If number is 1
-////1.2 - Current field resets to zero and change players turn.
+////1.2 - Current field resets to zero and change players turn. DONE
 ////If player presses hold btn
 ////1.3 - Save displayed number from the current field in a variable. DONE
-////1.4 - Change players turn
+////1.4 - Change players turn DONE
+////1.5 - Do same functionalities for other player
 
 //2 - CHECK WHICH PLAYER IS ACTIVE
 ////2.1 -
@@ -19,88 +20,85 @@
 ////HOLD: Hold score value of each player
 
 //Saved Elements
-const diceField = document.querySelector('.dice');
-const btnRollDice = document.querySelector('.btn--roll');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-btnHold.addEventListener('click', changePlayer);
+const btnNew = document.querySelector('.btn--new');
+const diceField = document.querySelector('.dice');
+
+const totalScoreFieldPZero = document.querySelector('#score--0');
 const currentFieldPZero = document.querySelector('#current--0');
-const currentfieldPOne = document.querySelector('#current--0');
 const playerZero = document.querySelector('.player--0');
+
+const totalScoreFieldPOne = document.querySelector('#score--1');
+const currentfieldPOne = document.querySelector('#current--1');
 const playerOne = document.querySelector('.player--1');
 
 //Current and Total Score for PlayerZero
-let currentScorePZero = 0;
+diceField.classList.add('hidden');
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 let totalScorePZero = 0;
-
-//Current and Total Score for Player One
-let currentScorePOne = 0;
 let totalScorePOne = 0;
 
-//Function to generate random dice number
-btnRollDice.addEventListener('click', () => {
-  const rollDice = Math.ceil(Math.random() * 6);
-  displayDiceValue(rollDice);
+//Funtion to roll dice and generate random Number
+btnRoll.addEventListener('click', function () {
+  //Generate random number
+  const dice = Math.ceil(Math.random() * 6);
+  //Display Dice image on the screen
+  diceField.classList.remove('hidden');
+  diceField.src = `dice-${dice}.png`;
+  console.log(`Roll: ${dice}`);
+
+  //Check if 'dice' is 1 or not.
+  if (dice !== 1) {
+    currentScore += dice;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    document.querySelector(`#score--${activePlayer}`).textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
+  }
 });
 
-function displayDiceValue(rollDice) {
-  switch (rollDice) {
-    case 1:
-      diceField.src = 'dice-1.png';
-      currentScorePZero = 0;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 1 - YOU LOST YOUR POINTS`);
-      changePlayer();
-      break;
-    case 2:
-      diceField.src = 'dice-2.png';
-      currentScorePZero += rollDice;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 2`);
-      break;
-    case 3:
-      diceField.src = 'dice-3.png';
-      currentScorePZero += rollDice;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 3`);
-      break;
-    case 4:
-      diceField.src = 'dice-4.png';
-      currentScorePZero += rollDice;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 4`);
-      break;
-    case 5:
-      diceField.src = 'dice-5.png';
-      currentScorePZero += rollDice;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 5`);
-      break;
-    case 6:
-      diceField.src = 'dice-6.png';
-      currentScorePZero += rollDice;
-      currentFieldPZero.textContent = currentScorePZero;
-      console.log(`Current score: ${currentScorePZero}`);
-      console.log(`Number: 6`);
-      break;
-  }
-}
+btnHold.addEventListener('click', function () {
+  diceField.classList.add('hidden');
+  document.querySelector(`#score--${activePlayer}`).textContent = currentScore;
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  console.log(scores);
+});
 
-//Function to change players turn
-function changePlayer() {
-  if (playerZero.classList.contains('player--active')) {
-    playerZero.classList.remove('player--active');
-    playerOne.classList.add('player--active');
-    console.log('Class removed from P Zero');
-    console.log('Player ONE ACTIVE');
-  } else {
-    playerOne.classList.remove('player--active');
-    playerZero.classList.add('player--active');
-    console.log('Class removed from P One');
-    console.log('Player ZERO ACTIVE');
-  }
-}
+// // Function to change players turn
+// function changePlayer() {
+//   if (playerZero.classList.contains('player--active')) {
+//     playerZero.classList.remove('player--active');
+//     playerOne.classList.add('player--active');
+
+//     totalScorePZero = rollDice;
+//     currentFieldPZero.textContent = 0;
+//   } else {
+//     playerOne.classList.remove('player--active');
+//     playerZero.classList.add('player--active');
+
+//     totalScorePOne = rollDice;
+//     currentfieldPOne.textContent = 0;
+//   }
+// }
+
+// function playerZeroFN(rollDice) {
+//   currentScore += rollDice;
+//   currentFieldPZero.textContent = currentScore;
+// }
+// function playerOneFN(rollDice) {
+//   currentScore += rollDice;
+//   currentfieldPOne.textContent = currentScore;
+// }
